335d1181b5df3973d6bdc481599bae0e7d8b8085
Amir Shahheydari <amir.shahheydari@appfolio.com>
2024-02-03 20:19:10 -0800

    am- persistent darkmode

--- src/components/DisplayModeToggle.js
+++ src/components/DisplayModeToggle.js
@@ -1,27 +1,37 @@
-import React , {useState} from 'react';
+import React , {useEffect, useState} from 'react';
 import Form from 'react-bootstrap/Form';
 
 
 export default function DisplayModeToggle() {
-    const [darkMode, setDarkMode] = useState(true)
+    const [darkMode, setDarkMode] = useState(
+        () => {
+            return localStorage.getItem('darkmode') === 'true' ? true : false
+        }
+    )
 
-
-    function toggleMode(e) {
-        setDarkMode(prevVal => !prevVal);
-
+    useEffect(() => {
         const html = document.getElementsByTagName("html")[0];
         if (darkMode) {
             html.setAttribute('data-bs-theme', 'dark');
         } else {
             html.setAttribute('data-bs-theme', 'light');
         }
+    });
+
+    useEffect(() => {
+        localStorage.setItem('darkmode', darkMode)
+    }, [darkMode]);
+
+    function toggleMode(e) {
+        setDarkMode(!darkMode);
     }
 
     return <Form style = {{ justifyContent:'right', float: 'right' }}>
-                <Form.Check
+                 <Form.Check
                     type="switch"
                     id="custom-switch"
                     onChange={toggleMode}
+                    checked={!darkMode}
                 />
            </Form>
 }
